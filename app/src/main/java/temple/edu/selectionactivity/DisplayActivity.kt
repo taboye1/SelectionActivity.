d8ef537e65e67800d722ccee4bdf09f674165672
taboye1 <taccp21@gmail.com>
2021-10-03 11:37:44 -0400

    Initial commit

---
+++ app/src/main/java/temple/edu/selectionactivity/DisplayActivity.kt
@@ -0,0 +1,11 @@
+package temple.edu.selectionactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
