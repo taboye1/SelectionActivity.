d515d89735ca7b857745c9f1354814713c808176
taboye1 <58162039+taboye1@users.noreply.github.com>
2021-10-14 00:32:35 -0400

    Update Picture.kt

--- app/src/main/java/temple/edu/selectionactivity/Picture.kt
+++ app/src/main/java/temple/edu/selectionactivity/Picture.kt
@@ -1,4 +1,26 @@
 package temple.edu.selectionactivity
 
-data class Picture(val name:String, val resourceId: Int)
+//data class Picture(val name:String, val resourceId: Int)
+data class Picture(val resourceId: Int, val description: String) : Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readInt(), parcel.readString().toString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) { parcel.writeInt(resourceId)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Picture> {
+       override fun createFromParcel(parcel: Parcel): Picture {
+            return Picture(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Picture?> {
+            return arrayOfNulls(size)
+        }
+   }
+}
 
